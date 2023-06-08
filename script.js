4b4ab666ed819b4d3bcb02a545c80142d88652f7
KatyW <ktabe13@gmail.com>
2023-06-08 18:31:54 -0500

    adding fortune cookie api

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-var fortuneBtn = document.querySelector('#fortuneBtn');
-
-fortuneBtn.addEventListener('click', function() {
-    console.log('fortuneBtn works');
-    document.location.replace('./thirdpage.html');
-})
-
 const settings = {
 	async: true,
 	crossDomain: true,
