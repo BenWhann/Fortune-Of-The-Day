13bcc6b07369bad038af39c9631cd909a076e838
KatyW <ktabe13@gmail.com>
2023-06-13 21:04:07 -0500

    adding comments

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -3,19 +3,18 @@ var imageBox = document.querySelector('.imageTest');
 var fortuneText = document.querySelector('#fortuneText');
 
 var dayJs = dayjs();
-var today = dayJs.format('MM/DD');
+var today = dayJs.format('dddd');
 console.log(today);
-
-fortuneBtn.addEventListener('click', function () {
-    console.log('fortuneBtn works');
-    document.location.replace('./thirdpage.html');
-})
-
 function DailyFortune(weekDay, fortune) {
     this.weekDay = weekDay;
     this.fortune = fortune;
 };
 var storedFortuneArray = JSON.parse(localStorage.getItem("Fortunes")) || [];
+
+fortuneBtn.addEventListener('click', function () {
+    console.log('fortuneBtn works');
+    document.location.replace('./thirdpage.html');
+})
 
 firstApiCall();
 
@@ -31,16 +30,16 @@ function firstApiCall() {
             'X-RapidAPI-Host': 'fortune-cookie2.p.rapidapi.com'
         },
         success: function (data) {
-            console.log(data);
+            //set fortunetext to returned fortune from API call
             fortuneText.textContent = data.answer;
-            console.log(data.category);
             var newFortune = new DailyFortune(today, data.answer);
-            
+     
             if (storedFortuneArray != null) {
+                //check if any object in array contains date value. If a fortune already exists for that day, we don't want
+                //to add another. Only 1 fortune per day.
                 var containsDate = storedFortuneArray.some(obj => obj["weekDay"] === today);
                 if(!containsDate)
                 {
-                    console.log("todays date not stored");
                     storedFortuneArray.push(newFortune);
                     localStorage.setItem("Fortunes", JSON.stringify(storedFortuneArray));
                 }
@@ -49,12 +48,12 @@ function firstApiCall() {
                 storedFortuneArray.push(newFortune);
                 localStorage.setItem("Fortunes", JSON.stringify(storedFortuneArray));
             }
-            console.log(storedFortuneArray);
+            //make call to second function, call second API
             secondApiCall(data.category);
         },
         error: function (jqXHR, textStatus, errorThrown) {
             if (jqXHR.status === 429) {
-                // Handle 429 status code here -- too mayn api requests
+                // Handle 429 status code here -- too many api requests
                 console.error('Too Many Requests:', jqXHR.responseText);
             } else {
                 // Handle other errors here
@@ -68,14 +67,13 @@ function firstApiCall() {
 
 
 function secondApiCall(category) {
-    console.log("here");
-    console.log(category);
-
+    //if category returned from first API call is general fortune, pass fortune to next API call instead due to limited results
     if (category == "General fortune") {
         category = "Fortune";
     }
 
-    var queryURL = 'http://api.giphy.com/v1/stickers/search?q=' + category + '&limit=5&api_key=wZmi04Bta6Iozo1cs6TlyPaawcltolg1';
+    //query string: limiting to return 5 result and rating is g or pg, search by category returned from first api call
+    var queryURL = 'http://api.giphy.com/v1/stickers/search?q=' + category + '&limit=5&rating=pg&api_key=wZmi04Bta6Iozo1cs6TlyPaawcltolg1';
     //handles ajax request to Giphy API
     $.ajax({
         url: queryURL, method: 'GET', success: function (data) {
@@ -84,6 +82,7 @@ function secondApiCall(category) {
             const shuffled = [...data.data].sort(() => 0.5 - Math.random());
             var resultArray =  shuffled.slice(0, 2);
             resultArray.forEach(element => {
+                //create new image element and append to imageBox section
                 imageBox.appendChild(new Image()).src = element.images.fixed_height.url;
             });
         },
