50c979d5b412f1b4451032fd44fe0ddd223e43de
BenWhann <whann004@umn.edu>
2023-06-15 20:44:47 -0500

    3rd page MVP finished

--- assets/js/script3.js
+++ assets/js/script3.js
@@ -1,5 +1,7 @@
 var fortuneContainer = document.querySelector('#fortunes-container');
 var cardContainer = document.querySelector('#cards-container');
+var cardContainer2 = document.querySelector('#cards-container-2');
+var cardBackground = document.querySelector('#card-background');
 
 var storedFortuneArray = JSON.parse(localStorage.getItem("Fortunes"));
 
@@ -13,11 +15,16 @@ storedFortuneArray.forEach(element => {
     fortuneContainer.appendChild(newDiv);
 
     var newDiv2 = document.createElement("div");
-    newDiv2.textContent = element.fortune;
+    var span = document.createElement('span');
+    span.textContent = element.fortune;
     newDiv2.setAttribute("class", "card");
     newDiv2.classList.add("hide");
     newDiv2.setAttribute("id","card-"+i);
-    cardContainer.appendChild(newDiv2);
+    span.setAttribute('class', 'span');
+    span.classList.add('hide');
+    span.setAttribute('id', 'span-'+i);
+    cardContainer2.appendChild(newDiv2);
+    newDiv2.appendChild(span);
 
     var closeButton = document.createElement('button');
     closeButton.textContent = "Close Fortune";
@@ -25,7 +32,9 @@ storedFortuneArray.forEach(element => {
     closeButton.setAttribute('type', 'button');
     closeButton.classList.add("hide");
     closeButton.setAttribute('id', 'closeButton-'+i);
-    cardContainer.appendChild(closeButton);
+    cardContainer2.appendChild(closeButton);
+
+    cardBackground.setAttribute('class', 'hide');
 
     i++;
 });
@@ -34,174 +43,304 @@ storedFortuneArray.forEach(element => {
 var fortune1 = document.querySelector('#fortune-1');
 var card1 = document.querySelector('#card-1');
 var closeButton1 = document.querySelector('#closeButton-1');
+var span1 = document.querySelector('#span-1');
 
 fortune1.addEventListener("click", function(){
     card1.classList.remove('hide');
     closeButton1.classList.remove('hide');
+    span1.classList.remove('hide');
+    cardBackground.classList.remove('hide');
+
     card2.classList.add('hide');
     closeButton2.classList.add('hide');
+    span2.classList.add('hide');
+
     card3.classList.add('hide');
     closeButton3.classList.add('hide');
+    span3.classList.add('hide');
+
     card4.classList.add('hide');
     closeButton4.classList.add('hide');
+    span4.classList.add('hide');
+
     card5.classList.add('hide');
     closeButton5.classList.add('hide');
+    span5.classList.add('hide');
+
     card6.classList.add('hide');
     closeButton6.classList.add('hide');
+    span6.classList.add('hide');
+
     card7.classList.add('hide');
     closeButton7.classList.add('hide');
+    span7.classList.add('hide');
+
 })
 closeButton1.addEventListener("click", function(){
     card1.classList.add('hide');
     closeButton1.classList.add('hide');
+    span1.classList.add('hide');
+    cardBackground.classList.add('hide');
 })
 
 var fortune2 = document.querySelector('#fortune-2');
 var card2 = document.querySelector('#card-2');
 var closeButton2 = document.querySelector('#closeButton-2');
+var span2 = document.querySelector('#span-2');
 
 fortune2.addEventListener("click", function(){
     card2.classList.remove('hide');
     closeButton2.classList.remove('hide');
+    span2.classList.remove('hide');
+    cardBackground.classList.remove('hide');
+
     card1.classList.add('hide');
     closeButton1.classList.add('hide');
+    span1.classList.add('hide');
+
     card3.classList.add('hide');
     closeButton3.classList.add('hide');
+    span3.classList.add('hide');
+
     card4.classList.add('hide');
     closeButton4.classList.add('hide');
+    span4.classList.add('hide');
+
     card5.classList.add('hide');
     closeButton5.classList.add('hide');
+    span5.classList.add('hide');
+
     card6.classList.add('hide');
     closeButton6.classList.add('hide');
+    span6.classList.add('hide');
+
     card7.classList.add('hide');
     closeButton7.classList.add('hide');
+    span7.classList.add('hide');
+
 })
 closeButton2.addEventListener("click", function(){
     card2.classList.add('hide');
     closeButton2.classList.add('hide');
+    span2.classList.add('hide');
+    cardBackground.classList.add('hide');
 })
 
 var fortune3 = document.querySelector('#fortune-3');
 var card3 = document.querySelector('#card-3');
 var closeButton3 = document.querySelector('#closeButton-3');
+var span3 = document.querySelector('#span-3');
 
 fortune3.addEventListener("click", function(){
     card3.classList.remove('hide');
     closeButton3.classList.remove('hide');
+    span3.classList.remove('hide');
+    cardBackground.classList.remove('hide');
+
     card2.classList.add('hide');
     closeButton2.classList.add('hide');
+    span2.classList.add('hide');
+
     card1.classList.add('hide');
     closeButton1.classList.add('hide');
+    span1.classList.add('hide');
+
     card4.classList.add('hide');
     closeButton4.classList.add('hide');
+    span4.classList.add('hide');
+
     card5.classList.add('hide');
     closeButton5.classList.add('hide');
+    span5.classList.add('hide');
+
     card6.classList.add('hide');
     closeButton6.classList.add('hide');
+    span6.classList.add('hide');
+
     card7.classList.add('hide');
     closeButton7.classList.add('hide');
+    span7.classList.add('hide');
+
 })
 closeButton3.addEventListener("click", function(){
     card3.classList.add('hide');
     closeButton3.classList.add('hide');
+    span3.classList.add('hide');
+    cardBackground.classList.add('hide');
 })
 
 var fortune4 = document.querySelector('#fortune-4');
 var card4 = document.querySelector('#card-4');
 var closeButton4 = document.querySelector('#closeButton-4');
+var span4 = document.querySelector('#span-4');
 
 fortune4.addEventListener("click", function(){
     card4.classList.remove('hide');
     closeButton4.classList.remove('hide');
+    span4.classList.remove('hide');
+    cardBackground.classList.remove('hide');
+
     card2.classList.add('hide');
     closeButton2.classList.add('hide');
+    span2.classList.add('hide');
+
     card3.classList.add('hide');
     closeButton3.classList.add('hide');
+    span3.classList.add('hide');
+
     card1.classList.add('hide');
     closeButton1.classList.add('hide');
+    span1.classList.add('hide');
+
     card5.classList.add('hide');
     closeButton5.classList.add('hide');
+    span5.classList.add('hide');
+
     card6.classList.add('hide');
     closeButton6.classList.add('hide');
+    span6.classList.add('hide');
+
     card7.classList.add('hide');
     closeButton7.classList.add('hide');
+    span7.classList.add('hide');
+
 })
 closeButton4.addEventListener("click", function(){
     card4.classList.add('hide');
     closeButton4.classList.add('hide');
+    span4.classList.add('hide');
+    cardBackground.classList.add('hide');
+
 })
 
 var fortune5 = document.querySelector('#fortune-5');
 var card5 = document.querySelector('#card-5');
 var closeButton5 = document.querySelector('#closeButton-5');
+var span5 = document.querySelector('#span-5');
 
 fortune5.addEventListener("click", function(){
     card5.classList.remove('hide');
     closeButton5.classList.remove('hide');
+    span5.classList.remove('hide');
+    cardBackground.classList.remove('hide');
+
     card2.classList.add('hide');
     closeButton2.classList.add('hide');
+    span2.classList.add('hide');
+
     card3.classList.add('hide');
     closeButton3.classList.add('hide');
+    span3.classList.add('hide');
+
     card4.classList.add('hide');
     closeButton4.classList.add('hide');
+    span4.classList.add('hide');
+
     card1.classList.add('hide');
     closeButton1.classList.add('hide');
+    span1.classList.add('hide');
+
     card6.classList.add('hide');
     closeButton6.classList.add('hide');
+    span6.classList.add('hide');
+
     card7.classList.add('hide');
     closeButton7.classList.add('hide');
+    span7.classList.add('hide');
+
 })
 closeButton5.addEventListener("click", function(){
     card5.classList.add('hide');
     closeButton5.classList.add('hide');
+    span5.classList.add('hide');
+    cardBackground.classList.add('hide');
+
 })
 
 var fortune6 = document.querySelector('#fortune-6');
 var card6 = document.querySelector('#card-6');
 var closeButton6 = document.querySelector('#closeButton-6');
+var span6 = document.querySelector('#span-6');
 
 fortune6.addEventListener("click", function(){
     card6.classList.remove('hide');
     closeButton6.classList.remove('hide');
+    span6.classList.remove('hide');
+    cardBackground.classList.remove('hide');
+
     card2.classList.add('hide');
     closeButton2.classList.add('hide');
+    span2.classList.add('hide');
+
     card3.classList.add('hide');
     closeButton3.classList.add('hide');
+    span3.classList.add('hide');
+
     card4.classList.add('hide');
     closeButton4.classList.add('hide');
+    span4.classList.add('hide');
+
     card5.classList.add('hide');
     closeButton5.classList.add('hide');
+    span5.classList.add('hide');
+
     card1.classList.add('hide');
     closeButton1.classList.add('hide');
+    span1.classList.add('hide');
+
     card7.classList.add('hide');
     closeButton7.classList.add('hide');
+    span7.classList.add('hide');
+
 })
 closeButton6.addEventListener("click", function(){
     card6.classList.add('hide');
     closeButton6.classList.add('hide');
+    span6.classList.add('hide');
+    cardBackground.classList.add('hide');
+
 })
 
 var fortune7 = document.querySelector('#fortune-7');
 var card7 = document.querySelector('#card-7');
 var closeButton7 = document.querySelector('#closeButton-7');
+var span7 = document.querySelector('#span-7');
 
 fortune7.addEventListener("click", function(){
     card7.classList.remove('hide');
     closeButton7.classList.remove('hide');
+    span7.classList.remove('hide');
+    cardBackground.classList.remove('hide');
+
     card2.classList.add('hide');
     closeButton2.classList.add('hide');
+    span2.classList.add('hide');
+
     card3.classList.add('hide');
     closeButton3.classList.add('hide');
+    span3.classList.add('hide');
+
     card4.classList.add('hide');
     closeButton4.classList.add('hide');
+    span4.classList.add('hide');
+
     card5.classList.add('hide');
     closeButton5.classList.add('hide');
+    span5.classList.add('hide');
+
     card6.classList.add('hide');
     closeButton6.classList.add('hide');
+    span6.classList.add('hide');
+
     card1.classList.add('hide');
     closeButton1.classList.add('hide');
+    span1.classList.add('hide');
+
 })
 closeButton7.addEventListener("click", function(){
     card7.classList.add('hide');
     closeButton7.classList.add('hide');
+    span7.classList.add('hide');
+    cardBackground.classList.add('hide');
+
 })
